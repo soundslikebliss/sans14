3f75bb3a1c57c82f51ee10feb2337006b8bd88d7
soundslikebliss <russianid1@gmail.com>
2014-09-16 18:32:47 +0000

    adds jquery scroll

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 
-
 $(document).foundation();
+$("#click").click(function () {
+    return $("html, body").animate({
+        scrollTop: $("#top").offset().top
+    }, 700);
+});
